f14a63263ee2ca109e67cbcdc3663757fe7b6e44
Ezequiel García <ezequiel.garcia@plyom.com.br>
2016-07-07 09:39:40 -0300

    create and add application

---
+++ app/src/main/java/com/example/ezequielgarcia/cameraandroid/applications/CameraApplication.java
@@ -0,0 +1,14 @@
+package com.example.ezequielgarcia.cameraandroid.applications;
+
+import android.app.Application;
+
+/**
+ * Created by ezequiel.garcia on 7/7/16.
+ */
+
+public class CameraApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
